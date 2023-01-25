8a743c4d51d83ccf96242ddc82f694e0a1a04586
eliotrineau <eliot.rineau@edu.ece.fr>
2023-01-25 14:35:04 +0100

    pioche fini

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 
 int main(){
-    //int x, y;
-    //Tuile Plateau[12][26];
-    //initialiserPlateau(Plateau);
-    //afficherPlateautest(Plateau);
-    //printf("x");
-    //scanf("%d", &x)
-    //printf("y");
-    //scanf("%d", &y)
-    //ajouterTuile(Plateau,x,y,couleur, M)
-    pioche();
+    //Tuile pioche[35];
+    //pioche(pioche);
     return 0;
 }
+
+//int x, y;
+//Tuile Plateau[12][26];
+//initialiserPlateau(Plateau);
+//afficherPlateautest(Plateau);
+//printf("x");
+//scanf("%d", &x)
+//printf("y");
+//scanf("%d", &y)
+//ajouterTuile(Plateau,x,y,couleur, M)
